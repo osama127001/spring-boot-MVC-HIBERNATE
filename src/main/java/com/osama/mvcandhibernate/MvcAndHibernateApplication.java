139fcc3c98c57468f5399c32740c005d8214defd
Osama Khan <52834187+gitosamakhan@users.noreply.github.com>
2021-02-03 16:47:34 +0500

    initial commit

---
+++ src/main/java/com/osama/mvcandhibernate/MvcAndHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.osama.mvcandhibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAndHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAndHibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/osama/mvcandhibernate/MvcAndHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osama.mvcandhibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAndHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
